49d73ddf88724c058e16300ab153bedb5bac7a37
Harshit Jain <harshit@hyperteam.co.in>
2022-01-20 01:06:32 +0530

    CPH2293: fod >>> udfps over oplus biometrics
     * wrap 2.1 hal and add additional function
     * wrap oplus biometrics service and advertise as 2.3
     * Remove lineage fod hal
     * Rename fodextenssion to udfpsextension
    
    * It's useless to have 2 services working along, with this commit we make rid of old 2.1 impl and merge it within 2.3
    * Use ontouchup instead of onfigerup
    * Similiar to dev-harsh1998/android_device_realme_X2@27a13d5 but not the same
    
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Co-authored-by: ThatisJigen <gianfranco.liguori.96@gmail.com>
    Co-authored-by: ManshuTyagi <himanshut567@gmail.com>
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: I18ccfb9a66c008dbe6189e1b238057835501cecc
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- device.mk
+++ device.mk
@@ -54,8 +54,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.CPH2293 \
-    lineage.biometrics.fingerprint.inscreen@1.0-service.CPH2293
+    android.hardware.biometrics.fingerprint@2.3-service.CPH2293
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
